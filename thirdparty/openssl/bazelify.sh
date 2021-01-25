eac9e3842f9ba0d791c062c5aaaaffeede37ffc7
James Sharpe <james.sharpe@zenotech.com>
2021-01-25 16:08:11 +0000

    Arm64 build files

--- thirdparty/openssl/bazelify.sh
+++ thirdparty/openssl/bazelify.sh
@@ -9,15 +9,15 @@ pushd openssl-$version
 # Define a new configuration that hard disables all dlopening.
 cat <<'E_O_F' >dbx.conf
 (
-    'dbx-linux-x86_64' => {
-        inherit_from    => [ 'linux-x86_64' ],
+    'dbx-linux-aarch64' => {
+        inherit_from    => [ 'linux-aarch64' ],
         dso_scheme     => undef,
     }
 );
 E_O_F
 # no-afalgeng because we don't need afalgeng.
 # no-dynamic-engine to prevent loading shared libraries at runtime.
-./Configure "--config=dbx.conf" dbx-linux-x86_64 no-afalgeng no-dynamic-engine
+./Configure "--config=dbx.conf" dbx-linux-aarch64 no-afalgeng no-dynamic-engine
 make include/openssl/opensslconf.h include/crypto/bn_conf.h include/crypto/dso_conf.h apps/progs.h
 echo -e "# BEGIN GENERATED CODE (see $(basename $0))\n" > "$mydir/BUILD.openssl"
 echo -e "OPENSSL_VERSION = \"$version\"\n" >> "$mydir/BUILD.openssl"
